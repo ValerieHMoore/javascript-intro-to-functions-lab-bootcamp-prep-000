715833cbfdd96350fcd503e00d4a0942c110ad17
Valerie Moore <periwinkle17@outlook.com>
2018-04-09 18:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if ('hello') {
     return 'I can\'t hear you!
-  else if ('HELLO!') {
+  else if ('HELLO') {
     return 'YES INDEED!'
   else if ('I love you, Grandma.')
   return ('I love you, too.')
