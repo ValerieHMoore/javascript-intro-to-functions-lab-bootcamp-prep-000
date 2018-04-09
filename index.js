9116da234fd43996c5e6918dfc46dcab021ddaeb
Valerie Moore <periwinkle17@outlook.com>
2018-04-09 18:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function sayHiToGrandma(string) {
   } else if (string === 'HELLO') { 
     return 'YES INDEED!';
 	}
-    else if (string === "I love you, Grandma.") return ('"I love you, too."');   
+    else if (string === "I love you, Grandma.") return ('I love you, too.');   
 }
