2a471781ee7df9061e1d59449c501c318d977e00
Valerie Moore <periwinkle17@outlook.com>
2018-04-09 18:45:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if str.toLowerCase() {
     return 'I can\'t hear you!
   } else if string.toUpperCase() {
     return 'YES INDEED!'
